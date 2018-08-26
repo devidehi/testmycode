a5c7cae4cb8a736e58d636304a4971969be8d4c7
devi <devidehi1@in.ibm.com>
2018-08-26 12:54:04 +0530

    add first file of app

---
+++ user_status.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+users = [
+	{'admin': True, 'active': True, 'name': 'Devi'},
+    {'admin': True, 'active': False, 'name': 'Amit'},
+	{'admin': False, 'active': True, 'name': 'Vivek'},
+	{'admin': False, 'active': False, 'name': 'Jyothi'},
+	{'admin': True, 'active': True, 'name': 'Smitha'},
+]
+
+line = 1
+for user in users: 
+	if user['admin'] and user['active']:
+		print("%s ACTIVE - (ADMIN) %s" % (line, user['name']))
+	elif user['admin']:
+		print("%s (ADMIN) %s" % (line, user['name']))
+	elif user['active']:
+		print("%s ACTIVE - %s" % (line, user['name']))
+	else:
+		print("%s %s" % (line, user['name']))
+	line += 1
+
+
