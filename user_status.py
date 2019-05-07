e9f061a36d49829cd241689134c6c2d995afc7e7
Devi Videhi <devidehi@google.com>
2019-05-07 12:26:35 +0530

    Deleted Hanuman

--- user_status.py
+++ user_status.py
@@ -10,7 +10,6 @@ users = [
 	{'admin': True, 'active': True, 'name': 'Ravi'},
 	{'admin': True, 'active': True, 'name': 'Ram'},
 	{'admin': True, 'active': True, 'name': 'Sita'},
-	{'admin': True, 'active': True, 'name': 'Hanuman'},
 ]
 
 line = 1
